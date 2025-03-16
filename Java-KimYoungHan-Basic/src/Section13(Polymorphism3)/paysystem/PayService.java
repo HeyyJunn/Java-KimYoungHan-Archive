f3612cbdc624aeadbb37f911d0d1dbb6d40f1381
xxanecdote@naver.com <xxanecdote@naver.com>
2025-03-16 20:26:54 +0900

    Update Java_KimYoungHan_Archive

--- Java-KimYoungHan-Basic/src/Section13(Polymorphism3)/paysystem/PayService.java
+++ Java-KimYoungHan-Basic/src/Section13(Polymorphism3)/paysystem/PayService.java
@@ -5,9 +5,17 @@ public class PayService {
         boolean result;
         System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
 
-        if (option.equals("kakao")) {
 
+        Pay pay = PayStore.findPay(option);
+        result = pay.pay(amount);
+
+        if (result) {
+            System.out.println("결제가 성공했습니다.");
+        } else {
+            System.out.println("결제가 실패했습니다.");
         }
+
+
 
     }
 }
